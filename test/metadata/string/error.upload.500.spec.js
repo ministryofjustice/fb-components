f23d670a05823d8adb7c53212e568dd55de3f7a4
Jonathan Perry <jonathanperry@sequencemedia.net>
2019-12-11 14:51:52 +0000

    Unit tests

---
+++ test/metadata/string/error.upload.500.spec.js
@@ -0,0 +1,18 @@
+require('@ministryofjustice/module-alias/register')
+
+const Ajv = require('ajv')
+
+const {
+  expect
+} = require('chai')
+
+const schemas = require('~/test/schemas')
+
+const jsonSchema = require('~/specifications/string/error/string.error.schema.json')
+const dataObject = require('~/metadata/string/error.upload.500.json')
+
+const ajv = new Ajv({schemas})
+
+const validator = ajv.compile(jsonSchema)
+
+describe('~/metadata/string/error.upload.500.json', () => it('validates', () => expect(validator(dataObject)).to.be.true))
