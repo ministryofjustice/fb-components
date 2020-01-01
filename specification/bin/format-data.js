290951d2b095cf1d9852e3917791d176f8f139e8
Jonathan Perry <jonathanperry@sequencemedia.net>
2020-01-01 16:55:55 +0000

    Move `schemaUtils` from `fb-components` to `fb-runner-node`

--- specification/bin/format-data.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-const path = require('path')
-const getComponentsPath = require('./get-components-path')
-const shell = require('shelljs')
-
-const componentsPath = path.relative(process.cwd(), getComponentsPath()) || '.'
-
-shell.exec(`find ${componentsPath}/specifications/**/data -name "*.json" | xargs -P 20 -L 1 jsonlint -qi`)
--- specification/documentation/pattern.md
+++
@@ -1,8 +0,0 @@
----
-layout: layout-pane.njk
-title: Patterns
----
-
-Patterns are best practice design solutions for specific user-focused tasks.
-
-
