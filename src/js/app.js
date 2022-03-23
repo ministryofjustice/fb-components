7d66fa8e20f98a8cb6fc50359c2b5e20026fd1d7
brenetic <brendan.butler@digital.justice.gov.uk>
2022-03-23 18:27:05 +0000

    Allow users to accept or reject analytics cookies
    This provides the ability for users to Accept or Reject analytics
    cookies.
    
    It will also delete the GA cookies that it finds in the browser.

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,11 @@
 const GOVUKFrontend = require('govuk-frontend/govuk/all')
 const TimeoutWarning = require('./component/timeout-warning')
+const Analytics = require('./component/analytics')
 
 GOVUKFrontend.initAll()
 
 new TimeoutWarning(
   document.querySelector('[data-module="govuk-timeout-warning"]')
 ).init()
+
+window.analytics = Analytics
---
+++ src/js/component/analytics.js
@@ -0,0 +1,53 @@
+function accept (cookieName) {
+  setAnalyticsCookie(cookieName, 'accepted')
+  hideCookieMessage()
+  showMessage('accepted')
+}
+
+function reject (cookieName) {
+  setAnalyticsCookie(cookieName, 'rejected')
+  removeAnalyticsCookies()
+  hideCookieMessage()
+  showMessage('rejected')
+}
+
+function setAnalyticsCookie (cookieName, cookieValue) {
+  document.cookie = `${cookieName}=${cookieValue}; expires=${new Date(
+    new Date().getTime() + 1000 * 60 * 60 * 24 * 365
+  ).toUTCString()}; path=/`
+}
+
+function hideCookieMessage () {
+  document.getElementById('govuk-cookie-banner-message').style.display = 'none'
+}
+
+function showMessage (messageType) {
+  document.getElementById(`govuk-cookie-banner-message-${messageType}`).style.display = 'block'
+}
+
+function hideCookieBanner () {
+  document.getElementById('govuk-cookie-banner').style.display = 'none'
+}
+
+function removeAnalyticsCookies () {
+  const cookiePrefixes = ['_ga', '_gid', '_gat_gtag_', '_utma', '_utmb', '_utmc', '_utmz']
+  for (const cookie of document.cookie.split(';')) {
+    for (const cookiePrefix of cookiePrefixes) {
+      const cookieName = cookie.split('=')[0].trim()
+      if (cookieName.startsWith(cookiePrefix)) {
+        deleteCookie(cookieName)
+      }
+    }
+  }
+}
+
+function deleteCookie (cookieName) {
+  document.cookie = `${cookieName}=; Path=/; Domain=${location.hostname}; Expires=Thu, 01 Jan 1970 00:00:01 UTC;`
+}
+
+module.exports = {
+  accept: accept,
+  reject: reject,
+  hideCookieBanner: hideCookieBanner,
+  removeAnalyticsCookies: removeAnalyticsCookies
+}
